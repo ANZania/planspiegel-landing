a0d5541f9c1fc156c68e08eda23f36386647a108
mskKote <msk.vitaly@gmail.com>
2024-12-03 03:14:23 +0100

    feat: new logo & colors

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,11 @@
 import Image from 'next/image'
-import logo from '@/images/ps_logo.svg'
+import logo from '@/images/Planspiegel.svg'
 
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={`flex items-center ${className}`}>
-      <Image
-        src={logo}
-        alt="Planspiegel Logo"
-        width={32}
-        height={32}
-      />
-      <span className="ml-2 mt-1 text-2xl font-athletic">PLANSPIEGEL</span>
+      <Image src={logo} alt="Planspiegel Logo" width={128} height={32} />
+      {/* <span className="ml-2 mt-1 text-2xl font-athletic">PLANSPIEGEL</span> */}
     </div>
   )
 }
