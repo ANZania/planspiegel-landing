bf8fdd576bc0de4806715632174ffb431f143f14
mskKote <msk.vitaly@gmail.com>
2024-12-06 02:42:30 +0100

    fix: blog card grid

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -30,7 +30,7 @@ export default async function BlogPage() {
         <h1 className="mb-8 text-center text-4xl font-bold">Blog</h1>
         <ul className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
           {allArticles.map((article) => (
-            <li key={article.slug}>
+            <li key={article.slug} className="h-full">
               <BlogArticleCard article={article} />
             </li>
           ))}
--- src/components/BlogArticleCard.tsx
+++ src/components/BlogArticleCard.tsx
@@ -6,10 +6,10 @@ import { RegularMetaAttributes } from 'react-datocms'
 type Props = { article: Article }
 export function BlogArticleCard({ article }: Props) {
   return (
-    <div className="relative overflow-hidden rounded-lg border bg-white transition-shadow hover:shadow-lg">
+    <div className="relative flex h-full flex-col overflow-hidden rounded-lg border bg-white transition-shadow hover:shadow-lg">
       <Link
         href={`/blog/${article.slug}`}
-        className="pointer-events-auto block"
+        className="pointer-events-auto block flex-grow"
       >
         <Image
           layout="responsive"
@@ -17,18 +17,22 @@ export function BlogArticleCard({ article }: Props) {
           height={9}
           src={article.aiGeneratedImage.url}
           alt={article.aiGeneratedImage.alt}
+          className="aspect-video object-cover object-top"
         />
-        <div className="p-4">
-          <h2 className="text-lg font-semibold no-underline">{article.h1}</h2>
-          <p>
-            {
-              article.seo.find(
-                ({ attributes }) =>
-                  (attributes as RegularMetaAttributes)?.name === 'description',
-              )?.attributes?.content
-            }
-          </p>
-          <span className="no-underline">
+        <div className="flex flex-grow flex-col justify-between p-4">
+          <div>
+            <h2 className="text-lg font-semibold no-underline">{article.h1}</h2>
+            <p>
+              {
+                article.seo.find(
+                  ({ attributes }) =>
+                    (attributes as RegularMetaAttributes)?.name ===
+                    'description',
+                )?.attributes?.content
+              }
+            </p>
+          </div>
+          <span className="mt-4 text-sm text-gray-600 no-underline">
             {new Date(article.date).getDate()}.
             {new Date(article.date).getMonth()}.
             {new Date(article.date).getFullYear()}
